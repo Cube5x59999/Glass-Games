f7264d0d1f09f1f3a71dfd4a166db0c4dda7068e
Cube5x59999 <98256780+Cube5x59999@users.noreply.github.com>
2022-08-22 19:18:54 +0530

    Create TweenGui.lua
    need help

---
+++ TweenGui.lua
@@ -0,0 +1,6 @@
+-- i need help 
+--[[
+i need to make when hover in the button the button will tween size to be large and not hovering it back tween to original size
+(Parent of script : Button)
+(ScriptType: LocalScript)
+]]
